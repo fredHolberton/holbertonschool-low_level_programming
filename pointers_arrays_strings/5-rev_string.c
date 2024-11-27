392cb5d7bc67a2b80399ce447d14ea45711a0390
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-27 15:14:37 +0100

    Test 5 Fix 1

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,22 @@
 void rev_string(char *s)
 {
 	int i = 0;
-    int n = 0;
-    int j;
-    char temp;
+	int n = 0;
+	int j;
+	char temp;
 
 	while (s[n] != '\0')
 	{
 		n++;
 	}
-    j = n - 1;
+	j = n - 1;
 	while (i < n / 2) 
 	{
 		temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        i++;
-        j--;
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
 	}
 }
 
