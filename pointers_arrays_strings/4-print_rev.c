4dd0b59cdeb31c8624a3c64e5f0de18566dab972
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-27 12:43:28 +0100

    Test 4 Fix 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev -  prints a string, in reverse, followed by a new line
+ * print_rev - prints a string, in reverse, followed by a new line
  * @s: pointer of a char
  *
  */
@@ -11,6 +11,7 @@ void print_rev(char *s)
 
 	while (s[i] != '\0')
 	{
+		_putchar(s[i]);
 		i++;
 	}
 	while (--i >= 0)
