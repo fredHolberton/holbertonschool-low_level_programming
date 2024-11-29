1de465fb54ead9dad1a54df4e285ac88a991e5d8
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-29 16:28:47 +0100

    Test 7 Fix 8

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,19 +20,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new = add_dnodeint(**h, n);
+		new = add_dnodeint(&h, n);
 		return (new);
 	}
 	else 
 	{
-		if (idx = (unsigned int)dlistint_len(h))
+		if (idx == (unsigned int)dlistint_len(h))
 		{
-			new = add_dnodeint_end(**h, n);
+			new = add_dnodeint_end(&h, n);
 			return (new);
 		}
 		else
 		{
-			if (idx > (unsigned int)dlistint_len(h))
+			if (idx > (unsigned int)dlistint_len(*h))
 				return (NULL);
 		}
 	}
