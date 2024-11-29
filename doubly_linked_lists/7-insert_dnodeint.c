e0d9b7f6c0de742406701813c9513faff42ca9e8
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-29 14:24:27 +0100

    Test 7 Fix 1

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *  insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: pointer of the head of a doubly liked list
+ * @idx: index of the node, starting from 0
+ * @n: Value of the data of the new node to insert
+ *
+ * Return: The new node that is inserted
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = h;
+	dlistint_t *new;
+	unsigned int i;
+
+	if (h == NULL and idx != 0)
+		return (NULL);
+
+	for (i = 1; i <= idx; i++)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = temp;
+	new->prev = temp->prev;
+	temp->prev = new;
+
+	return (new);
+}
+
